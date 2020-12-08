6cc3c539fc6527ad3b6552f77f0f454f3e51bd7b
seokjin <sjkimg1004@gmail.com>
2020-12-08 09:46:44 +0900

    to serve in git

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
-module.exports = {
-    publicPath: '/[frontend]/'
-  }
+module.exports={
+    publicPath: process.env.NODE_ENV === 'production'
+    ? '/githubPageTest'
+    : '/',
+    outputDir:'docs'
+}
