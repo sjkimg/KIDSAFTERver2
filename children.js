466a8cb1904f3e51d997332905a022ac3933eca7
juwon-yoo <72877645+juwon-yoo@users.noreply.github.com>
2020-12-09 10:15:43 +0900

    Add files via upload

---
+++ children.js
@@ -0,0 +1,54 @@
+export default {
+    Children: [
+        {
+          score: 1,
+          localname: '김포시건강가정·다문화가족지원센터',
+          tel: '031-996-5922',
+          updated_at: null,
+          long: 37.64670155,
+          lat: 126.6201657
+        },
+        {
+          score: 2,
+          localname: '남양주시건강가정·다문화가족지원센터',
+          tel: '031-554-8211',
+          updated_at: null,
+          long: 37.61024594,
+          lat: 127.1719582
+        },
+        {
+          score: 3,
+          localname: '수원시건강가정지원센터',
+          tel: '031-245-1319',
+          updated_at: null,
+          long: 37.27501427,
+          lat: 127.0161237
+        }
+      ],
+      Content: [
+        {
+          user_id: 'h2',
+          score: 1,
+          sex: 'male',
+          content_id: '축구',
+          localname: 'OO축구장',
+          updated_at: null
+        },
+        {
+            user_id: 'h1',
+            score: 2,
+            sex: 'female',
+            content_id: '독서',
+            localname: 'VW도서관',
+            updated_at: null
+          },
+          {
+            user_id: 'h3',
+            score: 3,
+            sex: 'female',
+            content_id: '축구',
+            localname: 'OO축구장',
+            updated_at: null
+          },
+      ],
+  }
